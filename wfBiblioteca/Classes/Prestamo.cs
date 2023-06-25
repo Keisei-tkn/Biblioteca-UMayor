f20f64b060f95b8a9dfac985150a9404462af893
Keisei <felipebasauren6@gmail.com>
2023-06-25 03:42:20 -0400

    Some Fixes

---
+++ wfBiblioteca/Classes/Prestamo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wfBiblioteca.Classes
+{
+    class Prestamo
+    {
+        public string Id { get; set; }
+        public DateTime fechaPrestamo { get; set; }
+        public DateTime fechaDevolucion { get; set; }
+    }
+}
+ 
